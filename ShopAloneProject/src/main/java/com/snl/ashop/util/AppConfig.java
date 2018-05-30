92df4cec1fa3171c28772c9db3072d44b98da4bf
Songchanheum <39077239+Songchanheum@users.noreply.github.com>
2018-05-30 15:50:00 +0900

    web

--- ShopAloneProject/src/main/java/com/snl/ashop/util/AppConfig.java
+++ ShopAloneProject/src/main/java/com/snl/ashop/util/AppConfig.java
@@ -24,6 +24,24 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         // TODO Auto-generated method stub
         registry.addResourceHandler("/resources/**").addResourceLocations(
                 "/resources/");
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/images/**")) {
+            registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
+        }
+        if (!registry.hasMappingForPattern("/css/**")) {
+            registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+        }
+        if (!registry.hasMappingForPattern("/js/**")) {
+            registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        }
+        if (!registry.hasMappingForPattern("/app/**")) {
+            registry.addResourceHandler("/app/**").addResourceLocations("classpath:/app/");
+        }
+        if (!registry.hasMappingForPattern("/node_modules/**")) {
+            registry.addResourceHandler("/node_modules/**").addResourceLocations("classpath:/node_modules/");
+        }
     }
  
     @Bean
